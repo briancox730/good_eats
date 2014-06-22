c230bc392b6b0b418428bddeb8338e76f4887f0b
Brian Cox <briancox730@gmail.com>
2014-06-22 14:30:54 -0400

    added New Restaurant form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,11 +13,24 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.new
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      flash.now[:notice] = "Your restaurant was not succesfully submitted."
+      redirect_to @restaurant
+    end
   end
 
   def list_ten
     @restaurants = Restaurant.all.includes(:category).last(10)
+  end
+
+  private
+
+  def question_params
+    params.require(:restaurant).permit(:name, :address, :city, :state, :zipcode, :description, :category_id)
   end
 
 end
